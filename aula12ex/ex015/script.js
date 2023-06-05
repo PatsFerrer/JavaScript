5587fe89ddfbe24f92592f8deee4268cf1e05234
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-06-05 13:25:28 -0300

    ex idade

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano') //formulario ano
+    var res = document.querySelector('div#res')
+
+    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var gênero = ''
+        var img = document.createElement('img') //cria uma tag img
+        img.setAttribute('id', 'foto') // colocando id='foto' na img pelo js
+       
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // crianca
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // crianca
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
