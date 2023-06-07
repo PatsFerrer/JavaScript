fd7ca61f2f1e6b5079e9a50c0c11e80ea100cc9c
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-06-06 21:15:49 -0300

    while and do while

---
+++ aula13/while.js
@@ -0,0 +1,16 @@
+var c = 1                      // c de contador
+while (c <= 6) {               //tudo que escrever dentro do bloco, vai acontecer enquanto (c < 6) for verdade
+    console.log(`Passo ${c}`)
+    c++                         // c = c + 1
+}
+
+
+
+/*
+console.log('Tudo certo?')
+console.log('Tudo certo?')
+console.log('Tudo certo?')
+console.log('Tudo certo?')
+console.log('Tudo certo?')
+console.log('Tudo certo?')
+*/
