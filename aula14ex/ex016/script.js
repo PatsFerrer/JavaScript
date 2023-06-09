f8048a341000bebe86f7c4eec7a0f6354a4da79d
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-06-09 10:10:26 -0300

    iniciei desafio

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,10 @@
+var inicio = document.getElementById('inicio')
+var fim = document.getElementById('fim')
+var passo = document.getElementById('passo')
+
+var res = document.getElementById('res')
+
+function clicar() {
+
+    res.innerHTML = `Contando: <br> 👉 você clicou 🏁`
+}
